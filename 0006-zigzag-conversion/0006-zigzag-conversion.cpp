71061c7593649f0647df0c30f01cb7239b778086
Sharsss <shekharsharanya01@gmail.com>
2026-08-16 15:44:40 +0530

    Time: 6 ms (46.38%), Space: 14 MB (71.61%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        int col=0,i=0;
-        string s1="";
-        vector<vector<char>> matrix(numRows);
-        while(i<s.size()){
-            if(col%2==0){
-                for(int row=0;row<numRows && i<s.size();row++){
-                    matrix[row].push_back(s[i]);
-                    i++;
-                }
-            }
-            else{
-                for(int row=numRows-2;row>=1&& i<s.size();row--){
-                    matrix[row].push_back(s[i]);
-                    i++;
-                }
-            }
-            col++;
+        if (numRows==1 || numRows>=s.size()){
+            return s;
         }
-        for(int row=0;row<numRows;row++){
-               for(int colm=0;colm<matrix[row].size();colm++){
-                    s1+=matrix[row][colm];
-                    }
-            }
-            return s1;
-}
+        int row=0,direction=1;
+        vector<string>rows(numRows);
+        for(char ch:s){
+            rows[row]+=ch;
+            if(row==0) direction=1;
+            else if(row==numRows-1) direction=-1;
+            row+=direction;
+
+        }
+        string ans="";
+        for(string &r : rows){
+            ans+=r;
+        }
+        return ans;
+    }
 };
